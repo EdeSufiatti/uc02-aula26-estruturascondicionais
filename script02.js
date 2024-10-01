5dc902c1fbb8ee5b7f0ab78b79a77799bd2d1e1b
Ede Sufiatti <C:/Users/edemi>
2024-10-01 11:47:08 -0300

    c5 0110

--- script02.js
+++ script02.js
@@ -1,10 +1,10 @@
-// Solicitando o comando ao usuário
+
 const comando = prompt("Informe o comando para a lâmpada (ligar / desligar)")
 
-// Criando o elemento de imagem
+
 const elementoImagem = document.createElement("img")
 
-// Lógica para definir o caminho da imagem com base no comando
+
 switch (comando.toLowerCase()) {
     case "ligar":
         elementoImagem.src = "./assets/on.png"
@@ -17,9 +17,9 @@ switch (comando.toLowerCase()) {
         break;
 }
 
-// Verificando se a imagem foi configurada corretamente
+
 if (elementoImagem.src) {
-    // Verificando se já existe a div para a lâmpada, se não, cria uma
+   
     let lampContainer = document.getElementById("lampContainer")
     if (!lampContainer) {
         lampContainer = document.createElement("div")
@@ -27,9 +27,9 @@ if (elementoImagem.src) {
         document.body.appendChild(lampContainer)
     }
 
-    // Limpando o conteúdo da div antes de adicionar a nova imagem
+  
     lampContainer.innerHTML = ''
     
-    // Adicionando a imagem dentro da div
+    
     lampContainer.appendChild(elementoImagem)
 }
